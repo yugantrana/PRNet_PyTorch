dd6dbd011b095d14862060e588aebf3bad797acf
PC09 <parulchopra09@gmail.com>
2021-05-15 04:57:49 +0000

    adding mobilenet code

--- config/config.py
+++ config/config.py
@@ -6,16 +6,16 @@
 """
 
 FLAGS = {"start_epoch": 0,
-         "target_epoch": 500,
+         "target_epoch": 160,
          "device": "cuda",
          "mask_path": "./utils/uv_data/uv_weight_mask_gdh.png",
-         "lr": 0.0001,
-         "batch_size": 16,
+         "lr": 0.00001,
+         "batch_size": 32,
          "save_interval": 5,
          "normalize_mean": [0.485, 0.456, 0.406],
          "normalize_std": [0.229, 0.224, 0.225],
-         "images": "./results",
+         "images": "./mobresults",
          "gauss_kernel": "original",
-         "summary_path": "./prnet_runs",
+         "summary_path": "./mob_runs",
          "summary_step": 0,
          "resume": True}
